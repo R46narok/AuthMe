663953c6aa30547080c93d4aa51479c0b9fb93d0
R46narok <stanimirkolev04@gmail.com>
2022-02-05 18:54:58 +0200

    Completed the infrastructure for .NET and SQL Server (will add Spring and MySQL)

---
+++ depl/mssql.tf
@@ -0,0 +1,98 @@
+
+resource "kubernetes_persistent_volume_claim" "mssql-claim" {
+  metadata {
+    name = "mssql-claim"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "2Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment_v1" "mssql" {
+  metadata {
+    name = "mssql-depl"
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        "app" = "mssql"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "app" = "mssql"
+        }
+      }
+
+      spec {
+        container {
+          name  = "mssql"
+          image = "mcr.microsoft.com/mssql/server"
+          port {
+            container_port = 1433
+          }
+          env {
+            name  = "ACCEPT_EULA"
+            value = "Y"
+          }
+          env {
+            name  = "SA_PASSWORD"
+            value = "AuthMeSuperSecurePassword123"
+          }
+          volume_mount {
+            mount_path = "/var/opt/mssql/data"
+            name       = "mssqldb"
+          }
+        }
+        volume {
+          name = "mssqldb"
+          persistent_volume_claim {
+            claim_name = "mssql-claim"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "mssql_clusterip_srv" {
+  metadata {
+    name = "mssql-clusterip-srv"
+  }
+  spec {
+    type = "ClusterIP"
+    selector = {
+      "app" = "mssql"
+    }
+    port {
+      name        = "mssql"
+      protocol    = "TCP"
+      port        = 1433
+      target_port = 1433
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "mssql_load_balancer" {
+  metadata {
+    name = "mssql-loadbalancer"
+  }
+  spec {
+    type = "LoadBalancer"
+    selector = {
+      "app" = "mssql"
+    }
+    port {
+      protocol    = "TCP"
+      port        = 1433
+      target_port = 1433
+    }
+  }
+} 
